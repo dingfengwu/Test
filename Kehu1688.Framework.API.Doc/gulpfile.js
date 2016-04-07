74fc064c9b3b335d149a83d1ab4e3f1df7931efe
wdf <2850968286@qq.com>
2016-04-07 20:59:39 +0800

    go

--- Kehu1688.Framework.API.Doc/gulpfile.js
+++ Kehu1688.Framework.API.Doc/gulpfile.js
@@ -17,7 +17,9 @@ var gulp = require("gulp"),
 var paths = {
     webroot: "./wwwroot/",
     source: "./wwwroot/html/source/",
-    htmldir:"./wwwroot/html/"
+    htmldir: "./wwwroot/html/",
+    cssdir: "./wwwroot/css/",
+    jsdir:"./wwwroot/js/"
 };
 
 paths.js = paths.webroot + "js/**/*.js";
@@ -30,6 +32,7 @@ paths.md = paths.webroot + "html/source/**/*.md";
 
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatHtmlDest = paths.webroot + "html/*.min.css";
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -39,25 +42,31 @@ gulp.task("clean:css", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean:html", function (cb) {
+    rimraf(paths.concatHtmlDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css", "clean:html"]);
 
 gulp.task("min:js", function () {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
-        .pipe(gulp.dest("."));
+        .pipe(".");
 });
 
 gulp.task("min:css", function () {
     return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
-        .pipe(gulp.dest("."));
+        .pipe(".");
 });
 
 gulp.task("min:html", function () {
     return gulp.src([paths.html, "!" + paths.minHtml])
+        .pipe(concat(paths.concatHtmlDest))
         .pipe(htmlmin())
-        .pipe(gulp.dest("."));
+        .pipe(".");
 });
 
 gulp.task('md',function () {
