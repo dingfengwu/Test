81bda50ea202a5eff508aa17648d29006916bf15
Doqu <306287611@qq.com>
2016-04-28 14:32:36 +0800

    update cache --to--caches

---
+++ Kehu1688.Framework.Caches/CacheKeyConsole.cs
@@ -0,0 +1,52 @@
+﻿using Kehu1688.Framework.Base;
+using Kehu1688.Framework.Cache.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kehu1688.Framework.Cache
+{
+    public class CacheKeyConsole: IEntity<CacheKeyConsole>
+    {
+        public List<TableKeyModel> tables { get; set; }
+
+        public List<InfoKeyModel> info { get; set; }
+
+        public string Itme { get; set; }
+
+        /// <summary>
+        /// key生成
+        /// </summary>
+        /// <returns></returns>
+        public string GetKey()
+        {
+            string key = string.Empty;
+            if (tables.Count > 0)
+            {
+                foreach(var t in tables)
+                {
+                    key = key + t.ToString();
+                }
+            }
+            if (info.Count > 0)
+            {
+                foreach (var u in info)
+                {
+                    key = key + u.ToString();
+                }
+            }
+            key = key.TrimEnd('.');
+            key = $"{key}={Itme}";
+            return key;
+        }
+
+        public CacheKeyConsole()
+        {
+            tables = new List<TableKeyModel>();            
+            info = new List<InfoKeyModel>();
+            //此处随机生成
+            Itme = Guid.NewGuid().ToString().Substring(10,5);
+        }
+    }
+}
---
+++ Kehu1688.Framework.Caches/Model/InfoKeyModel.cs
@@ -0,0 +1,36 @@
+﻿using Kehu1688.Framework.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kehu1688.Framework.Cache.Model
+{
+    public class InfoKeyModel: IEntity<InfoKeyModel>
+    {
+        public IdType type;
+        public string id;
+
+        public override string ToString() => $"{type}-{id}.";
+    }
+
+    public enum IdType
+    {
+        /// <summary>
+        /// user
+        /// </summary>
+        U,
+        /// <summary>
+        /// App
+        /// </summary>
+        A,
+        /// <summary>
+        /// Module
+        /// </summary>
+        M,
+        /// <summary>
+        /// other
+        /// </summary>
+        O
+    }
+}
